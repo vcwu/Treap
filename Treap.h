bd3939241ffbe7c55eac2c229ff43c980debf7ed
Victoria Wu <victoria.chen.wu@gmail.com>
2012-07-01 15:45:49 -0500

    Updated method comments.

--- Treap.h
+++ Treap.h
@@ -1,25 +1,114 @@
+/**
+Victoria Wu
+CS 352
+
+Treap
+
+An implementation of a binary search tree that incorporates properties of a heap,
+preventing an extremely imbalanced tree.
+
+
+*/
+
+asdfasdfadsfasdf
+#include <queue>
+#include <stack>
 #include <iostream>
 using namespace std;
 
 
 template <class T>
+struct Node
+{
+	T meat;
+	bool deleted;
+	unsigned int priority;
+
+};
+
+template <class T>
 class Treap
 {
+	
 private:
+	Node<T>* root;
+
+	/**
+	all_traverse()
+
+	Traverses all nodes in the tree breadth first, disregarding 
+	deleted or not. Helper method for size(), deleted(), empty(), contains().
+
+	Depending on which method calls..
+	size()		returns how many un deleted
+	delted()	returns how many deleted
+	empty()		returns if empty (bool - 0,1)
+	contains()	returns if found (bool - 0,1)
+
+	*/
+	int all_traverse();
+
+
 public:
+
+
 	Treap() {}
-	Treap(Treap t);
+	Treap(Treap<T> other);
 	~Treap() {}
 	&Treap operator=(const &Treap t);
 
+	/**
+	insert(const &T meat)
+
+	Inserts a node into the tree. 
+	*/
 	void insert(const &T meat);
 
-	//Removes ite, throws TreapException if called on empty treap
+	/**
+	remove(const &T meat)
+
+	Removes node with the given value.
+	If no node with that value exists, does nothing.
+	If called on an empty Treap, throws TreapException.
+	*/
 	T remove(const &T meat);
+	
+	/**
+	remove_min()
+
+	Removes the node with the minumum value from the tree
+	and returns the removed value.
+	Throws TreapException if called on empty Treap.
+	*/
 	T remove_min();
+
+	
+	/**
+	remove_max();
+
+	Removes the node with the max value from the tree
+	and returns the removed value.
+	Throws TreapException if called on empty Treap.
+	*/
 	T remove_max();
-	T find_min() const; //throws treapexception
-	T find_max() const; //throws treapexception
+
+	/**
+	find_min()
+
+	Finds and returns the minimum value.
+	Throws TreapException if called on empty Treap.
+	*/
+	T find_min() const; 
+
+	/**
+	find_max()
+
+	Finds and returns the minimum value.
+	Throws TreapException if called on empty Treap.
+	*/
+	T find_max() const; 
+
+
 	bool contains(const &T meat) const;
 	bool empty() const;
 	int size() const;
@@ -29,7 +118,26 @@ public:
 	void traverse_inorder(&ostream o, char delim = '\n');
 	void traverse_reverseorder(&ostream o, char delim = '\n');
 	void traverse_postorder(&ostream o, char delim = '\n');
-
-
-
+	asdfadfasdfasdf
 }
+/**
+all_traverse()
+
+Traverses the tree using a queue, depending on 
+what function called it will return different values. 
+*/
+template <class T>
+int Treap<T>::all_traverse()
+{
+	queue<Node<T>*> slimJim;		
+	slimJim.push(root);
+	
+}
+
+
+
+
+
+
+
+
